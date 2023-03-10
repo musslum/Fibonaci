aa51665397ea0b5fc934c228902558e7baa1e2b3
musslum <7nh8lxe@code.edu.az>
2023-03-10 18:44:09 +0400

    t

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task7_1
+{
+    internal class Program
+    {
+        static void Main(string[] args) // 1 1 2 3 5 8 13 21 34 55
+        {
+            int a = 1;
+            long b = 1;
+            long c = 1;
+            int n = 0;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            for (int i = 0; i < 8; i++)
+            {
+
+                c = c + b;
+                for (int k = 2; k < c; k++)
+                {
+                    if (c % k == 0)
+                    {
+                        n++;
+                    }
+                }
+                if (n == 0)
+                {
+                    Console.WriteLine(c);
+                }
+                n = 0;
+
+                b = c - b;
+
+
+            }
+
+        }
+
+
+
+    }
+}
